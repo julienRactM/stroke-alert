11f9084fa35124a0ea419a598b5dc484fd003890
julienRactM <julien.ract@sfr.fr>
2024-03-25 03:25:31 +0100

    [Update] Validation app/script

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import joblib
 import pandas as pd
+from script import model_prediction
 
 app = Flask(__name__)
 
@@ -11,28 +12,28 @@ def index():
     titles = "Allez vous subir un AVC ?"
 
     if request.method == 'POST':
-        model = joblib.load('app/app_model/current_model.pkl')
-        gender = request.form.get('gender')
+        # model = joblib.load('app/app_model/current_model.pkl')
         age = request.form.get('age')
-        bmi = request.form.get('bmi')
-        residence_type = request.form.get('residence_type')
+        hypertension = request.form.get('hypertension')
+        avg_glucose_level = request.form.get('avg_glucose_level')
+        heart_disease = request.form.get('heart_disease')
         smoking_status = request.form.get('smoking_status')
-        X_test = pd.DataFrame({'gender'         : [gender],
-                               'age'            : [age],
-                               'bmi'            : [bmi],
-                               'Residence_type' : [residence_type],
-                               'smoking_status' : [smoking_status]
-                               })
-        print(X_test)
-        prediction = model.predict(X_test)
+        X_test = pd.DataFrame({'age'            : [age],
+                        'hypertension'       : [hypertension],
+                        'avg_glucose_level'  : [avg_glucose_level],
+                        'heart_disease' : [heart_disease],
+                        'smoking_status' : [smoking_status]
+                        })
+        # print(X_test)
+        prediction = model_prediction(age, hypertension, avg_glucose_level, heart_disease, smoking_status)
 
         if prediction == 1 :
             comment = "L'algorithme détecte un risque d'AVC"
         else :
             comment = "L'algorithme ne prédit pas de risque accru d'AVC"
 
-        return render_template('index.html', titles = titles, prediction=prediction, comment=comment, gender=gender, \
-            age=age, bmi=bmi, Residence_type=residence_type, smoking_status=smoking_status) # active_tab='home'
+        return render_template('index.html', titles = titles, prediction=prediction, comment=comment, age=age,\
+            hypertension=hypertension, avg_glucose_level=avg_glucose_level, heart_disease=heart_disease, smoking_status=smoking_status) # active_tab='home'
 
 
     return render_template('index.html') # active_tab='home'
--- app/script.py
+++ app/script.py
@@ -1,12 +1,11 @@
 import sys
-import pickle
 import pandas as pd
+import joblib
 
-
-def model_predict(age, hypertension, avg_glucose_level, heart_disease, smoking_status):
-    # model = pickle.load('app/app_model/best_rf_model.pkl')
-    with open('app/app_model/best_rf_model.pkl', 'rb') as f:
-        model = pickle.load(f)
+def model_prediction(age, hypertension, avg_glucose_level, heart_disease, smoking_status):
+    model = joblib.load('app/app_model/model.pkl')
+    # with open('app/app_model/best_rf_model.pkl', 'rb') as f:
+    #     model = pickle.load(f)
     X_test = pd.DataFrame({'age'            : [age],
                        'hypertension'       : [hypertension],
                        'avg_glucose_level'  : [avg_glucose_level],
@@ -14,11 +13,11 @@ def model_predict(age, hypertension, avg_glucose_level, heart_disease, smoking_s
                        'smoking_status' : [smoking_status]
                        })
 
-    # return model.predict(X_test)
+    return model.predict(X_test)
 
 def error_message():
     print("""Il faut entrer en ligne de commande :
-        python script.py age(float range[10-120]) hypertension(int 1=True) avg_glucose_level(float range [50 300]) heart_disease(int 1=True) smoking_status(str smokes - formerly smoked - never smoked)""")
+        python script.py age(float range[1-120]) hypertension(int 1=True) avg_glucose_level(float range [50 300]) heart_disease(int 1=True) smoking_status(str smokes - formerly smoked - never smoked)""")
     return
 
 def main():
@@ -33,7 +32,7 @@ def main():
         error_message()
         return
 
-    prediction = model_predict(age, hypertension, avg_glucose_level, heart_disease, smoking_status)
+    prediction = model_prediction(age, hypertension, avg_glucose_level, heart_disease, smoking_status)
 
     if prediction == 0:
         return print("Pas de risque accru d'avc détecté")
@@ -41,9 +40,6 @@ def main():
         return print("Risque d'avc détecté")
     else:
         return print("Invalid prediction")
-    # return prediction
-
-
 
 if __name__ == "__main__":
     main()
